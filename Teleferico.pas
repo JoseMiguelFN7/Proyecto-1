a0f9b40082ac08191cae62274e0a7deb89abd71e
Jose Ferreira <jferreira.5655@unimar.edu.ve>
2023-10-24 16:12:00 -0400

    Menu principal con los 3 apartados agregado, con la opcion de salir del programa.

--- Teleferico.pas
+++ Teleferico.pas
@@ -1,7 +1,35 @@
 program teleferico;
 uses crt;
 var
-
+conExit: boolean;
+op: char;
 begin
-    
+    clrscr;
+    writeln('Bienvenido al sistema de teleferico de Merida!');
+    conExit:=false;
+    repeat
+        writeln('Por favor indique la opcion que desea realizar.');
+        writeln('1. Comprar boleto.');
+        writeln('2. ver Sistema.');
+        writeln('3. Salir.');
+        op:=readkey;
+        case (op) of
+            '1': begin
+                ////////////////////////////
+            end;
+            '2': begin
+                ////////////////////////////
+            end;
+            '3': begin
+                conExit:=true;
+                writeln('Gracias por usar el sistema! Vuelva pronto.');
+            end
+            else begin
+                writeln('El dato ingresado no es valido.');
+                delay(2000);
+                clrscr;
+            end;
+        end;
+    until conExit;
+    readkey;
 end.
